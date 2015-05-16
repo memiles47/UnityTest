e8422e0f6ef07085b737f546c15d9000e9bfb5dc
Michael Miles <boat081062@gmail.com>
2015-05-16 14:53:49 -0500

    Re-Add Repository
    Added repository to GigHub again.

---
+++ Assets/Test Scripts/HoldCharacter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoldCharacter : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            col.transform.parent = gameObject.transform;
+            //Debug.Log("On Platform");
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            col.transform.parent = null;
+            //Debug.Log("Off Platform");
+        }
+    }
+}
---
+++ Assets/Test Scripts/MovingPlatform.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatform : MonoBehaviour
+{
+    // Declaration of public variables
+    public Transform platform;
+    public Transform startPosition;
+    public Transform endPosition;
+    public float platformSpeed;
+
+    // Declaration of private variables
+    private Vector3 direction;
+    private Transform destination;
+
+    void Start()
+    {
+        SetDestination(startPosition);
+    }
+    
+    void FixedUpdate()
+    {
+        platform.GetComponent<Rigidbody>().MovePosition(platform.position + direction * platformSpeed * Time.fixedDeltaTime);
+
+        if(Vector3.Distance(platform.position, destination.position) < platformSpeed * Time.fixedDeltaTime)
+        {
+            SetDestination(destination == startPosition ? endPosition : startPosition);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(startPosition.position, platform.localScale);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(endPosition.position, platform.localScale);
+    }
+
+    void SetDestination(Transform dest)
+    {
+        destination = dest;
+        direction = (destination.position - platform.position).normalized;
+    }
+
+}
