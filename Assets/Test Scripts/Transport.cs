5cd7baa08cb5fad97a7b653bfac6924cf44840fb
Michael Miles <boat081062@gmail.com>
2015-06-14 08:23:31 -0500

    Minor changes
    Sometimes shit will not work so you come up with another way and then
    try the old way again and it works. What the heck! Oh well I got to
    shorten up my transporter script.

--- Assets/Test Scripts/Transport.cs
+++ Assets/Test Scripts/Transport.cs
@@ -4,14 +4,12 @@ using System.Collections;
 public class Transport : MonoBehaviour
 {
     // Declaration of private reference veribles
-    private Transform playerTrans;
     private Collider playerCol;
     private Vector3 destinationPos;
 
     // Awake is called when the script instance is being loaded
     public void Awake()
     {
-        playerTrans = GameObject.FindGameObjectWithTag("Player").transform;
         playerCol = GameObject.FindGameObjectWithTag("Player").GetComponent<Collider>();
         destinationPos = GameObject.FindGameObjectWithTag("DestOne").transform.position;
     }
@@ -19,9 +17,10 @@ public class Transport : MonoBehaviour
     // OnTriggerEnter is called when the Collider other enters the trigger
     public void OnTriggerEnter(Collider other)
     {
+        
         if (other = playerCol)
         {
-            playerTrans.position = new Vector3(destinationPos.x, destinationPos.y, destinationPos.z);
+            other.transform.position = destinationPos;
         }
     }
 }
